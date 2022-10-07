c94e82657745865ec7462fea09ef665b8422f475
Rafael Rocha <rafaelcruzrocha30@outlook.com>
2022-10-07 14:17:29 -0300

    Change plot to main

--- plot/graph/models.py
+++ plot/graph/models.py
@@ -6,22 +6,15 @@ class Pedido(models.Model):
     def get_pedidos(self, query=""):
         
         # Select query
-        query += "SELECT id, COUNT(id) as qtd, SUM(total) as faturamento, YEAR(created_at) as year FROM assinaturas_faturas"
-        
-        # Filter query
-        query += " WHERE status NOT IN(1,4)"
-        
-        # And filter
-        query += " AND total < 300"
-        
-        # Group query
-        query += " GROUP BY year"
-        
-        # Order query
-        query += " ORDER BY year"
-        
-        # Limit query
-        # query += " LIMIT 5"
+        query = """ 
+                    SELECT id, COUNT(id) as qtd, SUM(total) as faturamento, YEAR(created_at) as year 
+                    FROM assinaturas_faturas
+                    WHERE status NOT IN(1,4)
+                    AND total < 300
+                    GROUP BY year
+                    ORDER BY year  
+                    LIMIT 5
+                """
         
         return Pedido.objects.raw(query)
         
